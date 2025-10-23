9fab5bfa64bcb1874ada905b4ea9b9bf92f6d1ff
Muzafer <muzafermuz8006@gmail.com>
2025-10-23 11:40:57 +0530

    android & capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'markit',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: "#3880ff",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      spinnerStyle: "large",
+      spinnerColor: "#ffffff"
+    },
+  },
 };
 
 export default config;
+ 
